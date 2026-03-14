96fc9e44bf6afc060361861ddd6333837af4bc00
Minibobio <asgermaarbjerg@gmail.com>
2026-03-14 16:26:54 +0100

    test

--- Main.py
+++ Main.py
@@ -7,7 +7,7 @@ from Generator import etStep
 pygame.init()
 maze = []
 front = []
-front, maze = etStep(maze,front,500,500)
+front, maze = etStep(maze,front,100,100)
 
 screen = pygame.display.set_mode((width, height))
 
