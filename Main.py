e7fec67a253a1c140f0c11c9fecd6895755a0202
liamksimonsen <liamksimonsen@gmail.com>
2026-03-14 15:09:50 +0100

    visualisere generator

--- Main.py
+++ Main.py
@@ -2,12 +2,15 @@ import pygame, sys
 import time
 from Visuel import DrawMaze, height, width, color
 from Solver import MazeSolver
-from Generator import etStep
+from Generator import etStep, findfront, bro,tomt_board, startSlut
 
 pygame.init()
 maze = []
 front = []
-front, maze = etStep(maze,front,500,500)
+startX, startY = 5,5
+
+maze = tomt_board(100,100)
+front, maze = findfront(startY,startX,maze,front)
 
 screen = pygame.display.set_mode((width, height))
 
@@ -15,6 +18,13 @@ while True:
     screen.fill([0,0,0])
     for event in pygame.event.get():
         if event.type == pygame.QUIT: sys.exit()
-    MazeSolver(maze,screen)
+    
+    if len(front) > 0:
+        front, maze = bro(front, maze)
+        DrawMaze(maze,screen)
+        if len(front) == 0:
+            startSlut(startY,startX,maze)
+    else:
+        MazeSolver(maze,screen)
     pygame.display.flip()
 
